f5696778460bb45f25827d9cdf42a27e4fe4053e
Avi <95044844+Avioti@users.noreply.github.com>
2025-09-24 11:05:05 -0400

    Added scanner inputs and class

--- src/main/java/com/pluralsight/TemperatureConverter.java
+++ src/main/java/com/pluralsight/TemperatureConverter.java
@@ -1,11 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
 public class TemperatureConverter {
     public static void main(String[] args) {
 
-        double celsius = 23.7;
-        double fahrenheit = 98.6;
-        double kelvin = 300.15;
+        Scanner Enter_celsius = new Scanner(System.in);
+        System.out.println("Enter a Celsius temperature: ");
+        double celsius = Enter_celsius.nextDouble();
+        Scanner Enter_fahrenheit = new Scanner(System.in);
+        System.out.println("Enter a Fahrenheit temperature: ");
+        double fahrenheit = Enter_fahrenheit.nextDouble();
+        Scanner Enter_kelvin = new Scanner(System.in);
+        System.out.println("Enter a Kelvin temperature: ");
+        double kelvin = Enter_kelvin.nextDouble();
         double Converted_celsius = (celsius * 9 / 5) + 32;
         double Converted_fahrenheit = (fahrenheit - 32) * 5 / 9;
         double Celsus_to_kelvin = celsius + 273.15;
